d18f077ea0fc2a0edbd77c17d31a9c13b902d340
Ilya Petrov <ilya.muromec@gmail.com>
2014-12-22 01:39:00 +0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="zozol",
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/muromec/zozol',
     description='',
     author='Ilya Petrov',
