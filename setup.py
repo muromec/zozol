10e59bca545ba05e98eebdb796eb1e998bad60cc
Ilya Petrov <ilya.muromec@gmail.com>
2014-12-24 03:51:16 +0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="zozol",
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/muromec/zozol',
     description='Broken ASN1 for DSTU',
     author='Ilya Petrov',
